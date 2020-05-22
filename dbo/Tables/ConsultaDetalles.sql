9a6885472257f591afd7f82daab68b46b044aff3
BrunoMon <Brunomon@hotmail.com>
2020-05-22 12:53:34 -0300

    Modificacion Campos y Publicacion

--- dbo/Tables/ConsultaDetalles.sql
+++ dbo/Tables/ConsultaDetalles.sql
@@ -4,6 +4,7 @@
     [Diagnostico] VARCHAR (MAX) NULL,
     [IdEstado]    INT           NULL,
     [Activo]      INT           NULL,
+    [FechaUpdate] SMALLDATETIME NULL, 
     CONSTRAINT [PK_ConsultaDetalles] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_ConsultaDetalles_Consultas] FOREIGN KEY ([IdConsulta]) REFERENCES [dbo].[Consultas] ([Id])
 );
--- dbo/Tables/Consultas.sql
+++ dbo/Tables/Consultas.sql
@@ -6,6 +6,7 @@
     [Sintomas]          VARCHAR (100) NULL,
     [Fecha]             SMALLDATETIME NULL,
     [IdEstado]          INT           NULL,
+    [Activo] BIT NULL, 
     CONSTRAINT [PK_Consultas] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Consultas_MascotasUsuario] FOREIGN KEY ([IdMascotasUsuario]) REFERENCES [dbo].[MascotasUsuario] ([Id]),
     CONSTRAINT [FK_Consultas_MotivoConsultas] FOREIGN KEY ([IdMotivosConsulta]) REFERENCES [dbo].[MotivoConsultas] ([Id]),
